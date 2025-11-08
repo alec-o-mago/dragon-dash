a9736f131fba4511225fa08d63a00bff59230f42
Alec Santos <alecsantos96@gmail.com>
2025-11-08 14:26:38 -0300

    skins 16 to 36 and skin screen pagination

--- DragonDash/main/global.lua
+++ DragonDash/main/global.lua
@@ -1,9 +1,9 @@
 -- global.lua
 
--- module initialize
+-- Module initialize
 local M = {}
 
--- constants
+-- Constants
 M.SCREEN_WIDTH = 1280
 M.SCREEN_HEIGHT = 720
 M.MAX_EGGS = 30
@@ -23,28 +23,28 @@ M.LEVEL_COLORS = {
 	vmath.vector4(0.7, 0.7, 0.8, 1.0), -- gray
 }
 
--- variables
+-- Variables
 M.player_hearts = 5
-M.gold = 1000
+M.gold = 2000
 M.eggs = 3
 M.player_position = vmath.vector3(200, 360, 0)
 M.player_1_selected_skin = 3
 M.player_2_selected_skin = 2
 M.unlocked_skins = {
 	true, true, true, false, false, false, false, false, false, false,
-	false, false, false, false, false, --false, false, false, false, false,
-	--false, false, false, false, false, false, false, false, false, false,
+	false, false, false, false, false, false, false, false, false, false,
+	false, false, false, false, false, false, false, false, false, false,
 }
 M.high_score = {
 	bosses_defeated = 1,
-	time = 120, -- time in seconds
+	time = 120, -- Time in seconds
 }
-M.last_battle_result = { -- Example victory data
+M.last_battle_result = { -- Example battle data
 	bosses_defeated = 1,
-	time = 120, -- time in seconds
+	time = 120, -- Time in seconds
 }
 M.config_music = true
 M.config_sound = true
 
--- module return
+-- Module return
 return M
