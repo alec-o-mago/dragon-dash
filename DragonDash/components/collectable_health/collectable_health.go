8509e83287bceca7bde0ba770e69d5077cb9bebd
Alec Santos <alecsantos96@gmail.com>
2026-04-07 11:46:37 -0300

    luck level implemented

---
+++ DragonDash/components/collectable_health/collectable_health.go
@@ -0,0 +1,45 @@
+components {
+  id: "collectable_health"
+  component: "/components/collectable_health/collectable_health.script"
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"collectable_health\"\n"
+  "mask: \"player\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 37.5\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"heart\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/images/core/core.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    z: 0.7
+  }
+  scale {
+    x: 4.0
+    y: 4.0
+  }
+}
